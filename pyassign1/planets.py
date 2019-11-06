ed2b90ce08c9ce28cd801ef195913700df69d46b
Irving1113 <55149027+Irving1113@users.noreply.github.com>
2019-11-06 18:47:13 +0800

    Create planets.py
    提交作业

---
+++ pyassign1/planets.py
@@ -0,0 +1,32 @@
+import turtle
+import math
+
+sun = turtle.Pen()
+mercury = turtle.Pen()
+venus = turtle.Pen()
+earth = turtle.Pen()
+mars = turtle.Pen()
+jupiter = turtle.Pen()
+saturn = turtle.Pen()
+
+    
+def ellipse(a,b,t,color):
+    minAngle = (2*math.pi/360) * 360 / 50
+    t.color(color)
+    t.shape("circle") 
+    if i == 0:
+        t.penup()
+        t.setpos(0,-b)
+    else:
+        t.pendown()
+    nextPoint = [a*math.sin((i+1)*minAngle),-b*math.cos((i+1)*minAngle)]
+    t.setpos(nextPoint)  
+    
+for i in range(10000):
+    ellipse(0,0,sun,"yellow")
+    ellipse(57,34,mercury,"blue")
+    ellipse(108,65,venus,"green")
+    ellipse(149,90,earth,"red")
+    ellipse(227,136,mars,"black")
+    ellipse(278,167,jupiter,"orange")
+    ellipse(327,196,saturn,"brown")
