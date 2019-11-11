548a8aea86145dd2ad0147fa36c4b828c3984918
Irving1113 <55149027+Irving1113@users.noreply.github.com>
2019-11-11 23:13:13 +0800

    Update planets.py

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -1,45 +1,47 @@
 import turtle
 import math
 
-sun=turtle.Pen()
-mercury=turtle.Pen()
-venus=turtle.Pen()
-earth=turtle.Pen()
-mars=turtle.Pen()
-jupiter=turtle.Pen()
-saturn=turtle.Pen()
+sun = turtle.Pen()
+mercury = turtle.Pen()
+venus = turtle.Pen()
+earth = turtle.Pen()
+mars = turtle.Pen()
+jupiter = turtle.Pen()
+saturn = turtle.Pen()
 
-k=l=m=n=o=p=0
+k = r = m = n = o = p = 0
 
-def run(a,c,t,color,speed,angle):
+
+def run(a, c, t, color, speed, angle):
     t.shape('circle')
     t.color(color)
     t.speed(speed)
     if i == 0:
         t.up()
-        t.goto(a-c+111,0)
+        t.goto(a-c+111, 0)
     else:
         t.down()
-    b=(a**2-c**2)**(1/2)
-    angle=angle+speed
-    x=a*math.cos(math.radians(angle))-c+111
-    y=b*math.sin(math.radians(angle))
-    t.goto(x,y)
+    b = (a**2 - c**2)**(1/2)
+    angle = angle+speed
+    x = a*math.cos(math.radians(angle))-c+111
+    y = b*math.sin(math.radians(angle))
+    t.goto(x, y)
+
 
 for i in range(10000):
-    run(0,0,sun,'yellow',0,0)
-    run(57,11,mercury,'blue',15,k)
-    run(108,31,venus,'green',12,l)
-    run(149,54,earth,'red',9,m)
-    run(227,97,mars,'black',4,n)
-    run(278,132,jupiter,'orange',3,o)
-    run(327,177,saturn,'brown',1,p)
-    k=k+15
-    l=l+12
-    m=m+9
-    n=n+4
-    o=o+3
-    p=p+1
+    run(0, 0, sun, 'yellow', 0, 0)
+    run(57, 11, mercury, 'blue', 15, k)
+    run(108, 31, venus, 'green', 12, r)
+    run(149, 54, earth, 'red', 9, m)
+    run(227, 97, mars, 'black', 4, n)
+    run(278, 132, jupiter, 'orange', 3, o)
+    run(327, 177, saturn, 'brown', 1, p)
+    k = k+15
+    r = r+12
+    m = m+9
+    n = n+4
+    o = o+3
+    p = p+1
 
 if __name__ == '__main__':
-    main() 
+    main()
