8b8e896471fdb74d4e2fe3eeb1119ff6ebcfed96
Irving1113 <55149027+Irving1113@users.noreply.github.com>
2019-11-11 14:38:57 +0800

    Update planets.py

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -1,34 +1,45 @@
 import turtle
 import math
 
-sun = turtle.Pen()
-mercury = turtle.Pen()
-venus = turtle.Pen()
-earth = turtle.Pen()
-mars = turtle.Pen()
-jupiter = turtle.Pen()
-saturn = turtle.Pen()
-  
-def ellipse(a,b,t,color):
-    minAngle = (2*math.pi/360) * 360 / 50
+sun=turtle.Pen()
+mercury=turtle.Pen()
+venus=turtle.Pen()
+earth=turtle.Pen()
+mars=turtle.Pen()
+jupiter=turtle.Pen()
+saturn=turtle.Pen()
+
+k=l=m=n=o=p=0
+
+def run(a,c,t,color,speed,angle):
+    t.shape('circle')
     t.color(color)
-    t.shape("circle") 
+    t.speed(speed)
     if i == 0:
-        t.penup()
-        t.setpos(0,-b)
+        t.up()
+        t.goto(a-c+111,0)
     else:
-        t.pendown()
-    nextPoint = [a*math.sin((i+1)*minAngle),-b*math.cos((i+1)*minAngle)]
-    t.setpos(nextPoint)  
-    
+        t.down()
+    b=(a**2-c**2)**(1/2)
+    angle=angle+speed
+    x=a*math.cos(math.radians(angle))-c+111
+    y=b*math.sin(math.radians(angle))
+    t.goto(x,y)
+
 for i in range(10000):
-    ellipse(0,0,sun,"yellow")
-    ellipse(57,34,mercury,"blue")
-    ellipse(108,65,venus,"green")
-    ellipse(149,90,earth,"red")
-    ellipse(227,136,mars,"black")
-    ellipse(278,167,jupiter,"orange")
-    ellipse(327,196,saturn,"brown")
+    run(0,0,sun,'yellow',0,0)
+    run(57,11,mercury,'blue',15,k)
+    run(108,31,venus,'green',12,l)
+    run(149,54,earth,'red',9,m)
+    run(227,97,mars,'black',4,n)
+    run(278,132,jupiter,'orange',3,o)
+    run(327,177,saturn,'brown',1,p)
+    k=k+15
+    l=l+12
+    m=m+9
+    n=n+4
+    o=o+3
+    p=p+1
 
 if __name__ == '__main__':
-    main()
+    main() 
